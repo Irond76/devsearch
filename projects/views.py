cc2a150132a2a0880fc5d1e2f72ee9faf382f907
Dale Bendixen <dalebendixen@gmail.com>
2022-02-08 13:55:08 -0700

    added template inheritance

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def projects(request):
-    return HttpResponse('Here are our products')
+    return render(request, 'projects/projects.html')
+
 
 def project(request, pk):
-    return HttpResponse('Single Project' + " " + str(pk))
+    return render(request, 'projects/single-project.html')
